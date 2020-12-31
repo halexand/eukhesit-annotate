69544bf945464528d6815f6e988146de91391ca9
Harriet Alexander <harriet.xander@gmail.com>
2020-12-31 16:43:37 -0500

    cleanup

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -1,4 +1,4 @@
-snakemake   \
-        --jobs 100 --use-conda --rerun-incomplete \
+snakemake \
+        --jobs 800 --use-conda --rerun-incomplete \
         --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name={rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
 
